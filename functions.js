e673b40aef0e186053c279c6efc9396cbbfdc9ec
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2021-01-25 16:16:26 -0500

    class work done

---
+++ functions.js
@@ -0,0 +1,61 @@
+const identity = function (value) {
+  return value;
+};
+
+const indexOf = function (array, target) {
+  var result = -1;
+
+  each(array, function (item, index) {
+    if (item === target && result === -1) {
+      result = index;
+    }
+  });
+
+  return result;
+};
+
+const each = function (collection, iterator) {
+  if (collection instanceof Array) {
+    for (let i = 0; i < collection.length; i++) {
+      iterator(collection[i], i, collection);
+    }
+  } else if (collection instanceof Object) {
+    for (let prop in collection) {
+      iterator(collection[prop], prop, collection);
+    }
+  }
+};
+
+const filter = function (collection, test) {
+  let result = [];
+
+  each(collection, function(item) {
+    if (test(item)) {
+      result.push(item);
+    }
+  });
+  return result;
+};
+
+const map = function (collection, iterator) {};
+
+const reject = function (collection, test) {
+  let result = [];
+
+  filter(collection, function(item) {
+    if (!test(item)) {
+      result.push(item)
+    }
+  })
+  return result;
+};
+const uniq = function (array) {};
+const reduce = function (collection, iterator, accumulator) {};
+
+module.exports = {
+  filter,
+  reject,
+  uniq,
+  reduce,
+  map,
+};
